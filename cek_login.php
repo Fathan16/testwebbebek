0ab1a4202995c2491905227a07c9836669abfbd2
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-06 18:57:31 +0700

    Add files via upload

---
+++ cek_login.php
@@ -0,0 +1,47 @@
+<?php
+
+//aktif session
+session_start();
+
+require_once('db_login.php');
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//menyeleksi data user
+$login = $db->query("select * from akun where username='$username' and password='$password'");
+
+//menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+
+//cek apakah username dan password ditemukan pada database
+if($cek>0){
+
+    //memasukkan data dari login ke $data dalam bentuk array
+    $data=mysqli_fetch_assoc($login);
+
+    //cek jika user login sebagai admin
+    if($data['username']=="admin"){
+
+        //buat session login dan username
+        $_SESSION['username'] = $username;
+        //alihkan ke halaman dashboard admin
+        header("location:dashboard_admin.php");
+    }
+    else if($data['username']!="admin"){
+
+        //buat session login dan username
+        $_SESSION['username'] = $username;
+        //alihkan ke home customer
+        header("location:bookroom.php");
+    }
+    else{
+		// alihkan ke halaman login kembali
+        header("location:test.php");
+		//header("location:login.php?pesan=gagal");
+    }
+}
+else{
+    header("location:login.php?pesan=gagal");
+}
+?>
---
+++ get_ruangan.php
@@ -0,0 +1,60 @@
+<?php
+//kode feli
+  // require_once("db_login.php");
+
+  // if (isset($_GET["id_tipe"])) {
+  //   $id_tipe = $_GET["id_tipe"];
+
+  //   $query = "SELECT * FROM model WHERE id_tipe='" . $id_tipe . "'";
+  //   $result = $db->query($query);
+
+  //   if (!$result) {
+  //     die("Could not query the database: <br />" . $db->error);
+  //   }
+
+  //   echo '<option value="" selected>Pilih Room</option>';
+    
+  //   while ($row = $result->fetch_object()) {
+  //     echo '<option value="' . $row->id . '">' . $row->nama . '</option>';
+  //   }
+    
+  //   $result->free();
+  //   $db->close();
+?>
+<?php 
+// Punya Nada
+//   require_once("db_login.php");
+
+//   if (isset($_GET["id_tipe"])) {
+//     $id_tipe = $_GET["id_tipe"];
+
+//     $query = "SELECT * FROM ruang WHERE id_tipe='" . $id_tipe . "'";
+//     $result = $db->query($query);
+
+//     if (!$result) {
+//       die("Could not query the database: <br />" . $db->error);
+//     }
+
+//     echo '<option value="" selected>Pilih Room</option>';
+    
+//     while ($data = $result->fetch_object()) {
+//       echo '<option value="' . $data->id_tipe . '">' . $data->no_ruang . '</option>';
+//     }
+
+//     $result->free();
+//     $db->close();
+//   }
+// ?>
+<!-- Punya Patan -->  
+<?php 
+    require_once("db_login.php");
+
+  if (isset($_GET['id'])) {
+      $id = $_GET['id'];
+      $result = $db->query("SELECT * from ruang WHERE id_tipe='$id'");
+?>
+  <option value="0">-- Select Room Number --</option>
+  <?php while ($data = $result->fetch_object()): ?>
+      <option value="<?php echo $data->no_ruang ?>"><?php echo $data->no_ruang ?></option>
+  <?php endwhile;
+  }
